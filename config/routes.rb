5d2770077e49c12f6961d52ae7e7f917a72ec263
EC2 Default User <ec2-user@ip-172-31-15-49.ec2.internal>
2022-04-25 06:17:56 +0000

    [Add]bookのindex,show,edit機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  
   root to: 'homes#top'
   devise_for :users
   get "homes/about" => "homes#about", as: "about"
 
-  resources :books, only:[:new, :index, :show]
+  resources :books, only:[:new, :create, :index, :show, :edit, :destroy]
+
+  resources :users, only:[:show, :edti]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
