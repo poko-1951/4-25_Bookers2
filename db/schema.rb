1f8e74a0d74b10c5579d028ef8c91380ee3be22f
EC2 Default User <ec2-user@ip-172-31-15-49.ec2.internal>
2022-04-26 16:27:14 +0000

    [Fix]migrationのbookテーブルのopinionカラムをbodyカラムに変更,アイコン追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_143025) do
+ActiveRecord::Schema.define(version: 2022_04_26_153315) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2022_04_22_143025) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
-    t.text "opinion"
+    t.text "body"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
